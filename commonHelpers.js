c8cfc6173d990397ec60232d868c2907f98c96b3
KaratSergio <137483700+KaratSergio@users.noreply.github.com>
2023-12-08 08:17:02 +0000

    Deploying to gh-pages from @ KaratSergio/project-caramel@e8beaaab1708876a68013f79fe338b4d2e885b13 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,19 +1,19 @@
-import{a as d}from"./assets/vendor-a61d8330.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerpolicy&&(r.referrerPolicy=s.referrerpolicy),s.crossorigin==="use-credentials"?r.credentials="include":s.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(s){if(s.ep)return;s.ep=!0;const r=o(s);fetch(s.href,r)}})();const f="https://food-boutique.b.goit.study/api/";d.defaults.baseURL=f;async function m({keyword:t,category:e,page:o,limit:a,sort:s}={}){const r={keyword:t,category:e,page:o,limit:a};return s&&s.key&&s.value&&(r[s.key]=s.value),(await d.get(`${f}products`,{params:r})).data}const v=document.querySelector(".list-prod"),y={keyword:"",category:"",page:1,limit:9};function b(t,e){t.innerHTML=e}async function L(){try{const{results:t}=await m(y),e=$(t);b(v,e)}catch(t){console.error("Ошибка при отображении продуктов:",t)}}function $(t){return t.map(({id:e,name:o,img:a,category:s,size:r,price:n,popularity:c})=>`
-        <li class="prod-item" js-product-id=${e}>   
+import{a as c}from"./assets/vendor-a61d8330.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const l="https://food-boutique.b.goit.study/api/";c.defaults.baseURL=l;async function v({keyword:t,category:a,page:n,limit:s,sort:e}={}){const r={keyword:t,category:a,page:n,limit:s};return e&&e.key&&e.value&&(r[e.key]=e.value),(await c.get(`${l}products`,{params:r})).data}async function m(t){return(await c.get(`${l}products/popular`,{params:{limit:t}})).data}const y=document.querySelector(".list-prod"),L={keyword:"",category:"",page:1,limit:9};function b(t,a){t.innerHTML=a}async function k(){try{const{results:t}=await v(L),a=E(t);b(y,a)}catch(t){console.error("Ошибка при отображении продуктов:",t)}}function E(t){return t.map(({id:a,name:n,img:s,category:e,size:r,price:i,popularity:o})=>`
+        <li class="prod-item" js-product-id=${a}>   
           <div class="prod-pic">
             <svg class="discont-prod" width="60" height="60" style="visibility: hidden;">
               <use href="../images/icons.svg#shopping-cart"></use>
             </svg>
-            <img class="prod-img" src=${a} alt=${o} loading="lazy">
+            <img class="prod-img" src=${s} alt=${n} loading="lazy">
           </div>
-          <h3 class="title-prod">${o}</h3>
+          <h3 class="title-prod">${n}</h3>
           <div class="feature">
-            <p class="feature-prod">Category:<span class="feature-value">${s}</span></p>
+            <p class="feature-prod">Category:<span class="feature-value">${e}</span></p>
             <p class="feature-prod">Size:<span class="feature-value">${r}</span></p>
-            <p class="feature-prod push">Popularity:<span class="feature-value">${c}</span></p>
+            <p class="feature-prod push">Popularity:<span class="feature-value">${o}</span></p>
           </div>
           <div class="buing-prod">
-            <p class="price-prod"> &#36;${n}</p>
+            <p class="price-prod"> &#36;${i}</p>
             <button class="buy-btn" type="button">
               <svg class="buy-svg" width="18" height="18">
                 <use href="../images/icons.svg#shopping-cart"></use>"></use>
@@ -21,32 +21,40 @@ import{a as d}from"./assets/vendor-a61d8330.js";(function(){const e=document.cre
             </button>
           </div>
         </li>
-      `).join("")}L();async function g(t,e){if(paginationContainer.innerHTML="",e>1){for(let a=Math.max(1,t-1);a<=Math.min(e,t+1);a+=1)u(a,a===t);if(e-t>5-2){const a=document.createElement("span");a.textContent=". . .",paginationContainer.appendChild(a)}for(let a=Math.max(e-5+4);a<=e;a+=1)u(a)}}function u(t,e=!1){const o=document.createElement("li"),a=document.createElement("div");a.classList.add("pagination-link");const s=document.createElement("a");s.href="javascript:void(0);",s.textContent=t,e&&a.classList.add("active"),s.addEventListener("click",()=>S(t)),a.appendChild(s),o.appendChild(a),paginationContainer.appendChild(o)}function S(t){displayCards(t),g(t,60)}const k=1;g(k,60);const E=document.querySelector(".popular-list");async function A(){const t="https://food-boutique.b.goit.study/api/products/popular";return(await d.get(`${t}`)).data}function C(t){return t.map(({_id:e,name:o,img:a,category:s,popularity:r,size:n})=>{const c=parseInt(r.toString()[0]);return`  <li class="popular-item">
-        <button class="btn-add" type="submit">svg</button>
-        <span class="product-added" >OK</span>
-        <div class="popular-card" id="${e}">
+      `).join("")}k();async function g(t,a){if(paginationContainer.innerHTML="",a>1){for(let s=Math.max(1,t-1);s<=Math.min(a,t+1);s+=1)u(s,s===t);if(a-t>5-2){const s=document.createElement("span");s.textContent=". . .",paginationContainer.appendChild(s)}for(let s=Math.max(a-5+4);s<=a;s+=1)u(s)}}function u(t,a=!1){const n=document.createElement("li"),s=document.createElement("div");s.classList.add("pagination-link");const e=document.createElement("a");e.href="javascript:void(0);",e.textContent=t,a&&s.classList.add("active"),e.addEventListener("click",()=>$(t)),s.appendChild(e),n.appendChild(s),paginationContainer.appendChild(n)}function $(t){displayCards(t),g(t,60)}const S=1;g(S,60);const C=document.querySelector(".popular-list");function M(t){return t.map(({_id:a,name:n,img:s,category:e,popularity:r,size:i})=>{const o=parseInt(r.toString()[0]);return`  <li class="popular-item">
+      <button class="btn-add" type="button">
+        <svg class="svg-add" width="12" height="12">
+          <use href="./images/icons.svg#shopping-cart"></use>
+        </svg>
+      </button>
+      <span class="product-added">
+        <svg class="svg-added" width="12" height="12">
+          <use href="./images/icons.svg#check"></use>
+        </svg>
+      </span>
+        <div class="popular-card" id="${a}">
           <div class="popular-box-img">
-            <img src="${a}" alt="${o}" loading="lazy"  width="56" />
+            <img src="${s}" alt="${n}" loading="lazy"  width="56" />
           </div>
           <div class="popular-description">
-            <h3 class="popular-card-title">${o}</h3>
-            <p class="popular-card-text category">Category: <span class="popular-text">${s.replace("_"," ")}</span></p>
+            <h3 class="popular-card-title">${n}</h3>
+            <p class="popular-card-text category">Category: <span class="popular-text">${e.replace("_"," ")}</span></p>
             <div class="card-descr">
-              <p class="popular-card-text">Size: <span class="popular-text">${n}</span></p>
-              <p class="popular-card-text">Popularity: <span class="popular-text">${c}</span></p>
+              <p class="popular-card-text">Size: <span class="popular-text">${i}</span></p>
+              <p class="popular-card-text">Popularity: <span class="popular-text">${o}</span></p>
             </div>
           </div>
         </div>
-      </li>`}).join("")}A().then(t=>{E.insertAdjacentHTML("beforeend",C(t));const e={btnAdd:document.querySelector(".btn-add"),productAdded:document.querySelector(".product-added"),popularCard:document.querySelector(".popular-card")};e.btnAdd.addEventListener("click",o),e.popularCard.addEventListener("click",M),e.productAdded.classList.remove("product-added"),e.productAdded.classList.add("is-hidden");function o(){e.btnAdd.classList.add("is-hidden"),e.productAdded.classList.add("product-added"),e.productAdded.classList.remove("is-hidden"),alert("Product add to Order")}}).catch(t=>{console.error(t)});function M(){alert("Thi is product cart")}d.defaults.baseURL="https://food-boutique.b.goit.study/api/";function P(){return d.get("products/discount").then(t=>t.data)}const h="product-discount";function _(){try{const t=localStorage.getItem(h);return t?JSON.parse(t):[]}catch(t){console.lof(t.message)}}function l(t){localStorage.setItem(h,JSON.stringify(t))}function x(t=[]){return t.map(e=>` <div class="cart-product-discount" data-id="${e._id}">
+      </li>`}).join("")}m(5).then(t=>{C.insertAdjacentHTML("beforeend",M(t)),[...document.querySelectorAll(".popular-item")].forEach(n=>{const s=n.children[1];s.classList.add("visually-hidden");const e=n.children[0];e.addEventListener("click",r);function r(O){alert("Product add to Order"),e.classList.add("visually-hidden"),s.classList.remove("visually-hidden"),e.removeEventListener("click",r)}const i=n.children[2];i.addEventListener("click",o);function o(){alert("This is product cart"),i.removeEventListener("click",o)}})}).catch(t=>{console.error(t)});c.defaults.baseURL="https://food-boutique.b.goit.study/api/";function A(){return c.get("products/discount").then(t=>t.data)}const h="product-discount";function w(){try{const t=localStorage.getItem(h);return t?JSON.parse(t):[]}catch(t){console.lof(t.message)}}function f(t){localStorage.setItem(h,JSON.stringify(t))}function P(t=[]){return t.map(a=>` <div class="cart-product-discount" data-id="${a._id}">
     <div class="card-product-wrapper">
         <img class="cart-product-img"
-            src="${e.img}"
-            width="180" height="180" alt="${e.desc}" />
+            src="${a.img}"
+            width="180" height="180" alt="${a.desc}" />
     </div>
     <div class="card-product-info">
-        <h3 class="cart-product-title">${e.name}</h3>
+        <h3 class="cart-product-title">${a.name}</h3>
         <div class="card-product-info-right">
-            <p class="cart-product-price">${e.price}</p>
+            <p class="cart-product-price">${a.price}</p>
 
             <button type="button" class="cart-product-btn">
                 <svg>
@@ -56,5 +64,5 @@ import{a as d}from"./assets/vendor-a61d8330.js";(function(){const e=document.cre
         </div>
     </div>
 </div>
-`).join("")}let i=[];const p=document.querySelector(".products-discount");console.log(p);async function w(){i=(await P()).slice(0,2);const e=x(i);q(e)}w();function q(t){p.insertAdjacentHTML("beforeend",t)}p.addEventListener("click",I);function I(t){if(!t.target.closest(".cart-product-btn"))return;const o=t.target.closest(".cart-product-discount").dataset.id,a=_();if(a.find(s=>o===s._id))l(a.filter(s=>o!==s._id));else{const s=i.find(r=>o===r._id);a.push(s),l(a)}}
+`).join("")}let d=[];const p=document.querySelector(".products-discount");console.log(p);async function _(){d=(await A()).slice(0,2);const a=P(d);x(a)}_();function x(t){p.insertAdjacentHTML("beforeend",t)}p.addEventListener("click",I);function I(t){if(!t.target.closest(".cart-product-btn"))return;const n=t.target.closest(".cart-product-discount").dataset.id,s=w();if(s.find(e=>n===e._id))f(s.filter(e=>n!==e._id));else{const e=d.find(r=>n===r._id);s.push(e),f(s)}}
 //# sourceMappingURL=commonHelpers.js.map
