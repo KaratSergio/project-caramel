f7216636c690f94d12071cc2f7fa5428067bfa8a
mykolamytsyk <138970828+mykolamytsyk@users.noreply.github.com>
2023-12-09 22:33:58 +0200

    ss

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,6 @@
-const countProducts = document.getElementById('countProducts');
-// countProducts.innerHTML = `cart (${10})`;
-let count = 0;
-document.getElementById('cartLink').addEventListener('click', () => {
-  countProducts.innerHTML = `cart (${++count})`;
-});
-
-const newWord = 'Java Script';
-console.log(newWord.length - 1);
-console.log(newWord[newWord.length]);
+// const countProducts = document.getElementById('countProducts');
+// // countProducts.innerHTML = `cart (${10})`;
+// let count = 0;
+// document.getElementById('cartLink').addEventListener('click', () => {
+//   countProducts.innerHTML = `cart (${++count})`;
+// });
