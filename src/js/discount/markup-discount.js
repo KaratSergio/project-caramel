22c76754e9849df84778067d80395085475bbaf1
sashasashkina <koshyl.apps@gmail.com>
2023-12-10 14:19:55 +0200

    add commit seven

--- src/js/discount/markup-discount.js
+++ src/js/discount/markup-discount.js
@@ -29,26 +29,13 @@ export function createMarkup(items = [], idProducts = []) {
         <div class="card-product-info-right">
             <p class="card-product-price">${item.price}</p>
             <button type="button" class="card-product-btn" >
-<<<<<<< HEAD
             <svg class="card-product-svg ${check}" width="18" height="18">
             <use href="./images/icons.svg#check"></use>
-=======
-            <svg class="card-product-svg" width="18" height="18">
-            <use href="${sprite}#shopping-cart"></use>
->>>>>>> 562c6e1027d47fd4cc286998d94a8c91af1e9557
           </svg>
           <svg class="card-product-svg ${card}" width="18" height="18">
           <use href="./images/icons.svg#shopping-cart"></use>
         </svg>
             </button>
-<<<<<<< HEAD
-=======
-            <span class="product-added">
-            <svg class="svg-added" width="12" height="12">
-              <use href="${sprite}#check"></use>
-            </svg>
-          
->>>>>>> 562c6e1027d47fd4cc286998d94a8c91af1e9557
         </div>
     </div>
 </div>
